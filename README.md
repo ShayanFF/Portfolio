9ec146ccfccb582bf1a648036e2a411ca8f838c3
Shayan <shayan.firouzian@ryerson.ca>
2022-09-06 19:07:35 -0400

    move to typescript

--- README.md
+++
@@ -1,7 +0,0 @@
-Website Revamp
-
-Will use react for front-end and express for back-end
-
-End goal is to have contact requests sent to me via discord bot
-
-Currently a heavy WIP
--- src/components/LinkButton.js
+++
@@ -1,7 +0,0 @@
-const LinkButton = ({ onClick, text, image }) => {
-    return (
-        <a href={onClick}><img src={image} alt={text} className='header-btn' /></a>
-    )
-}
-
-export default LinkButton
