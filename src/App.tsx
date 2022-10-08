29023591c68c92e94834d706928516d82e334bfc
Shayan <55254368+ShayanFF@users.noreply.github.com>
2022-10-08 16:01:14 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-// import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import LoadingBar from "./components/LoadingBar";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
-const App = () => {
+const App: React.FC = () => {
+  const [navBarPosition, setNavBarPosition] = useState<string>("CENTER");
+
   return (
     <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path="/" component={App} />
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
-        </Routes>
-      </div>
+        <div className='app-container'>
+          <NavBar />
+          <Routes>
+            <Route path='/'></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/projects' element={<Projects />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+          </Routes>
+        </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
